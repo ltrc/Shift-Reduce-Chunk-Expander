99f6ffb07b0f020f46768007788f7c88294b5b34
riyazbhat <riyaz.ah.bhat@gmail.com>
2017-07-24 18:52:27 -0600

    added few rules to grammar and addressed processing of quotes.

--- src/chunk_expander.py
+++ src/chunk_expander.py
@@ -12,60 +12,61 @@ import argparse
 
 from arc_eager import arcEager
 from ssf_reader import SSFReader
-	
+    
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def getAttributeValuePairs(node):
-	pairs = list()
-	drel_ = list()
-	nodeDict = node._asdict()
-	for attribute in nodeDict:
-		value = nodeDict[attribute]
-		if (attribute != 'name') and (not value): continue
-		if attribute == "af": pairs.append("=".join(("af","'%s'" % ",".join([af_attr for af_attr in value]))))
-		elif attribute in ["id", "pos", "head"]: pairs.append(value)
-		elif attribute in ["drel", "parent"]: drel_.append(value) if attribute == "parent" else drel_.append(value)
-		else:pairs.append("=".join((attribute,"'%s'" % value)))
-	if drel_: pairs.insert(4, "drel='%s'" % ":".join(drel_))
-	return pairs
+    pairs = list()
+    drel_ = list()
+    nodeDict = node._asdict()
+    for attribute in nodeDict:
+        value = nodeDict[attribute]
+        if (attribute != 'name') and (not value): continue
+        if attribute == "af": pairs.append("=".join(("af","'%s'" % ",".join([af_attr for af_attr in value]))))
+        elif attribute in ["id", "pos", "head"]: pairs.append(value)
+        elif attribute in ["drel", "parent"]: drel_.append(value) if attribute == "parent" else drel_.append(value)
+        else:pairs.append("=".join((attribute,"'%s'" % value)))
+    if drel_: pairs.insert(4, "drel='%s'" % ":".join(drel_))
+    return pairs
 
 def backToSSF(tree_, sent_id):
-	outputFile.write("%s\n" % sent_id)
-	for treelet in tree_:
-		treelet	= treelet._replace(pid=None)
-		if treelet.chunkType == None:
-			treelet	= treelet._replace(chunkType="child:%s"%treelet.chunkId)
-			treelet = treelet._replace(chunkId=None)
-		else:
-			for headNode in tree_:
-				if headNode.chunkType == None:continue
-				if treelet.parent == headNode.chunkId:
-					treelet = treelet._replace(parent=headNode.name)
-		ssftreelet = getAttributeValuePairs(treelet)
-		outputFile.write("\t".join((ssftreelet[0].encode("utf-8"),ssftreelet[1].encode("utf-8"),
-			ssftreelet[2].encode("utf-8"), "<fs %s>\n" % " ".join(ssftreelet[3:]).encode("utf-8"))))
-	outputFile.write("</Sentence>\n\n")
-	logFile.write("%s Successfully expanded!!\n" %sent_id)
+    outputFile.write("%s\n" % sent_id)
+    for treelet in tree_:
+        treelet = treelet._replace(pid=None)
+        if treelet.chunkType == None:
+            treelet = treelet._replace(chunkType="child:%s"%treelet.chunkId)
+            treelet = treelet._replace(chunkId=None)
+        else:
+            for headNode in tree_:
+                if headNode.chunkType == None:continue
+                if treelet.parent == headNode.chunkId:
+                    treelet = treelet._replace(parent=headNode.name)
+        ssftreelet = getAttributeValuePairs(treelet)
+        outputFile.write("\t".join((ssftreelet[0].encode("utf-8"),ssftreelet[1].encode("utf-8"),
+            ssftreelet[2].encode("utf-8"), "<fs %s>\n" % " ".join(ssftreelet[3:]).encode("utf-8"))))
+    outputFile.write("</Sentence>\n\n")
+    logFile.write("%s Successfully expanded!!\n" %sent_id)
 
 def updateHead(head, headinfo, mapping, sentence=None):
-	infoDict = headinfo._asdict()
-	for attribute in infoDict:
-		if infoDict[attribute] or attribute == "chunkType":
-			if attribute == "name": 
-				head = head._replace(**{attribute:mapping[int(head.id)]})
-			elif attribute == "chunkType":
-				head = head._replace(**{attribute:"head:%s" % infoDict['chunkId']})
-			elif attribute == "head":
-				head = head._replace(**{attribute:head.head})
-			elif (headinfo.head != head.name) and (attribute == "af"):continue
-			else:
-				head = head._replace(**{attribute:infoDict[attribute]})
-	if (headinfo.head != head.name) and (head.head != 'NULL'):
-		logFile.write("Error: Computed head is probably wrong.\n")
-		logFile.write("%s\n" % sentence)
-	return head
+    #NOTE head.head is word and head.name is unique word instance (word, word2, word3 etc.)
+    infoDict = headinfo._asdict()
+    for attribute in infoDict:
+        if infoDict[attribute] or attribute == "chunkType":
+            if attribute == "name": 
+                head = head._replace(**{attribute:mapping[int(head.id)]})
+            elif attribute == "chunkType":
+                head = head._replace(**{attribute:"head:%s" % infoDict['chunkId']})
+            elif attribute == "head":
+                head = head._replace(**{attribute:head.head})
+            elif (headinfo.head != head.name) and (attribute == "af"):continue
+            else:
+                head = head._replace(**{attribute:infoDict[attribute]})
+    if (headinfo.head != head.name) and (head.head != 'NULL') and (None not in [headinfo.head,head.name]):
+        logFile.write("Error: Computed head is probably wrong in %s.\n" % sentence)
+        logFile.write("'%s' not equal to '%s'\n" % (headinfo.head.encode("utf-8"), head.name.encode("utf-8")))
+    return head
 
 def ilmtAPI(source, target, first, last, text):
         pool = urllib3.PoolManager()
@@ -76,96 +77,96 @@ def ilmtAPI(source, target, first, last, text):
         return json.loads(data)
 
 def headVibComputation(sentence, source, target):
-	#NOTE handled NULL nodes without af
-	nullHandled = re.sub(r"<fs name='NULL(.*?)'>",r"<fs af='null,unk,,,,,,' name='NULL\1'>", sentence.strip())
-	#NOTE compute head
-	if source == "hin":
-		headComputed = ilmtAPI(source, target, '11', '11', "input=%s" % (urllib.quote(nullHandled)))
-	else:
-		headComputed = ilmtAPI(source, target, '10', '10', "input=%s" % (urllib.quote(nullHandled)))
-	#payload = "keep=true&input=" + urllib.quote(headComputed['computehead-9'])
-	if source == "hin":
-		return headComputed['computehead-11']
-	else:
-		return headComputed['computehead-10']
+    #NOTE handled NULL nodes without af
+    nullHandled = re.sub(r"<fs name='NULL(.*?)'>",r"<fs af='null,unk,,,,,,' name='NULL\1'>", sentence.strip())
+    #NOTE compute head
+    if source == "hin":
+        headComputed = ilmtAPI(source, target, '11', '11', "input=%s" % (urllib.quote(nullHandled)))
+    else:
+        headComputed = ilmtAPI(source, target, '10', '10', "input=%s" % (urllib.quote(nullHandled)))
+    #payload = "keep=true&input=" + urllib.quote(headComputed['computehead-9'])
+    if source == "hin":
+        return headComputed['computehead-11']
+    else:
+        return headComputed['computehead-10']
 
 def backoff(chunk, head, index_mapping):
-	tree = []
-	if head.chunkId.startswith('FRAGP'): head = head._replace(head=chunk[0].head) 
-	headName = head.head
-	for nid, node in enumerate(chunk):
-		if node.head == head.head:
-			node = updateHead(node, head, index_mapping)
-		else:
-			node = node._replace(parent=headName, drel='mod')
-		tree.append(node)
-	return tree
+    tree = []
+    if head.chunkId.startswith('FRAGP'): head = head._replace(head=chunk[0].head) 
+    headName = head.head
+    for nid, node in enumerate(chunk):
+        if node.head == head.head:
+            node = updateHead(node, head, index_mapping)
+        else:
+            node = node._replace(parent=headName, drel='mod')
+        tree.append(node)
+    return tree
 
 def expander(sentences):
-	for idx,sentence in enumerate(sentences):
-		sent_id = sentence.group(1).replace("'", '"')
-		sentence_content = sentence.group(2)
-		sentence = "%s%s</Sentence>" % (sent_id, sentence_content)
-		try:
-			sentence = headVibComputation(sentence,source, target).encode("utf-8")
-		except:
-			logFile.write("%s -> Error: Something wrong in head or vibhakhti computation\n" % (sent_id))
-		logger.info("Sentence number {0} read for processing.".format(idx+1))
+    for idx,sentence in enumerate(sentences):
+        sent_id = sentence.group(1).replace("'", '"')
+        sentence_content = sentence.group(2)
+        sentence = "%s%s</Sentence>" % (sent_id, sentence_content)
+        try:
+            sentence = headVibComputation(sentence,source, target).encode("utf-8")
+        except:
+            logFile.write("%s -> Error: Something wrong in head or vibhakhti computation\n" % (sent_id))
+        logger.info("Sentence number {0} read for processing.".format(idx+1))
 
-		if re.search(r"comment=probsent",sentence):logFile.write("%s -> Error: Found a Probsent comment\n" % (sent_id));continue
-		try:
-			reader_object = SSFReader(sentence.strip().split("\n")[1:-1]).getAnnotations()
-		except:
-			logFile.write("%s -> Error: Probably wrong SSF format.\n" % (sent_id));continue
-		sanity_check = reader_object.treeSanity()
-		if sanity_check:logFile.write("%s -> Error: %s\n" % (sent_id, sanity_check));continue
-		tree_ = list()
-		for chunk in reader_object.nodeList:
-			sr_parser = arcEager(grammar, chunk[1:])
-			try:
-				sr_parser.parse()
-			except NotImplementedError:
-				unknownRule = [" ".join((cn.id,cn.head,cn.pos)) for cn in chunk[1:]]
-				logFile.write("Error: Unknown derivation %s\n" % ("|".join(unknownRule).encode("utf-8")))
-				logFile.write("%s\n" % sentence)
-				expandedNoise = backoff(chunk[1:], chunk[0], reader_object.nodeIndex)
-				tree_.extend(expandedNoise)
-			else:
-				expandedChunk = sr_parser.sequence
-				expandedChunk[sr_parser.stack[0]] = updateHead(expandedChunk[sr_parser.stack[0]], chunk[0], 
-									reader_object.nodeIndex, sentence)
-				for node_ in expandedChunk: tree_.append(node_)
-		if tree_:backToSSF(tree_, sent_id)
-	logFile.close()
-	outputFile.close()
+        if re.search(r"comment=probsent",sentence):logFile.write("%s -> Error: Found a Probsent comment\n" % (sent_id));continue
+        try:
+            reader_object = SSFReader(sentence.strip().split("\n")[1:-1]).getAnnotations()
+        except:
+            logFile.write("%s -> Error: Probably wrong SSF format.\n" % (sent_id));continue
+        sanity_check = reader_object.treeSanity()
+        if sanity_check:logFile.write("%s -> Error: %s\n" % (sent_id, sanity_check));continue
+        tree_ = list()
+        for chunk in reader_object.nodeList:
+            sr_parser = arcEager(grammar, chunk[1:])
+            try:
+                sr_parser.parse()
+            except NotImplementedError:
+                unknownRule = [" ".join((cn.id,cn.head,cn.pos)) for cn in chunk[1:]]
+                logFile.write("Error: Unknown derivation %s\n" % ("|".join(unknownRule).encode("utf-8")))
+                logFile.write("%s\n" % sentence)
+                expandedNoise = backoff(chunk[1:], chunk[0], reader_object.nodeIndex)
+                tree_.extend(expandedNoise)
+            else:
+                expandedChunk = sr_parser.sequence
+                expandedChunk[sr_parser.stack[0]] = updateHead(expandedChunk[sr_parser.stack[0]], chunk[0], 
+                                    reader_object.nodeIndex, sent_id)
+                for node_ in expandedChunk: tree_.append(node_)
+        if tree_:backToSSF(tree_, sent_id)
+    logFile.close()
+    outputFile.close()
 
 if __name__ == "__main__":
-	
-	parser = argparse.ArgumentParser(description="Chunk Expander !!")
-	parser.add_argument('--input-file'     , dest='input'     , required=True, help='Input file in ssf format')
-	parser.add_argument('--output-file'    , dest='output'    , required=True, help='Output file')
-	parser.add_argument('--grammar-file'   , dest='grammar'   , required=True, help='Grammar file')
-	parser.add_argument('--language'       , dest='language'  , required=True, choices=['hin', 'urd'], help='Input language')
-	parser.add_argument('--log-file'       , dest='log'       , required=True, help='will contain expansion details')
+    
+    parser = argparse.ArgumentParser(description="Chunk Expander !!")
+    parser.add_argument('--input-file'     , dest='input'     , required=True, help='Input file in ssf format')
+    parser.add_argument('--output-file'    , dest='output'    , required=True, help='Output file')
+    parser.add_argument('--grammar-file'   , dest='grammar'   , required=True, help='Grammar file')
+    parser.add_argument('--language'       , dest='language'  , required=True, choices=['hin', 'urd'], help='Input language')
+    parser.add_argument('--log-file'       , dest='log'       , required=True, help='will contain expansion details')
 
-	args = parser.parse_args()
-	
-	if os.path.isfile(os.path.abspath(args.output)): outputFile = open(args.output,'a')
-	else: outputFile = open(args.output,'w')
-	
-	if os.path.isfile(os.path.abspath(args.log)): logFile = open(args.log,'a')
-	else: logFile = open(args.log,'w')
+    args = parser.parse_args()
+    
+    if os.path.isfile(os.path.abspath(args.output)): outputFile = open(args.output,'a')
+    else: outputFile = open(args.output,'w')
+    
+    if os.path.isfile(os.path.abspath(args.log)): logFile = open(args.log,'a')
+    else: logFile = open(args.log,'w')
 
-	if args.language == "hin":	
-		source, target = args.language, "urd"
-	else:
-		source, target = args.language, "hin"
+    if args.language == "hin":  
+        source, target = args.language, "urd"
+    else:
+        source, target = args.language, "hin"
 
-	filePath = os.path.abspath(args.input)
-	logFile.write(filePath+"\n")
+    filePath = os.path.abspath(args.input)
+    logFile.write(filePath+"\n")
 
-	with open(args.grammar) as jfp: grammar = eval(jfp.read())#json.load(jfp)
-	with open(args.input) as inputFile:
-		inputFileString = inputFile.read()
-		sentences = re.finditer("(<Sentence id=.*?>)(.*?)</Sentence>", inputFileString, re.S)
-	expander(sentences)
+    with open(args.grammar) as jfp: grammar = eval(jfp.read())#json.load(jfp)
+    with open(args.input) as inputFile:
+        inputFileString = inputFile.read()
+        sentences = re.finditer("(<Sentence id=.*?>)(.*?)</Sentence>", inputFileString, re.S)
+    expander(sentences)
--- src/ssf_reader.py
+++ src/ssf_reader.py
@@ -9,90 +9,104 @@ from collections import namedtuple
 from sanity_checker import SanityChecker
 
 class SSFReader (SanityChecker):
+    
+    def __init__ (self, sentence):
+
+        self.id_ = int()
+        self.nodeList = list()
+        self.nodeIndex = dict()
+	self.instances = dict()
+        self.sentence = sentence
+        self.modifierModified = dict()
+        self.node = namedtuple('node', 
+                        ['id', 'head', 'pos', 'poslcat', 'af', 'vpos', 'name','drel','parent', 'pid',
+                        'chunkId', 'chunkType', 'mtype', 'troot', 'coref', 'stype','voicetype', 'posn'])
+        self.features = namedtuple('features',
+                            ['lemma','cat','gen','num','per','case','vib','tam'])
+
+    def getAnnotations (self):
+        children_ = list()
+        chunkId = str()
+        for line in self.sentence:
+            nodeInfo = line.decode("utf-8").split("\t")
+            if nodeInfo[0].isdigit(): # chunk
+                assert len(nodeInfo) == 4 # no need to process trash! FIXME
+                attributeValue_pairs = self.FSPairs(nodeInfo[3][4:-1])
+                attributes = self.updateFSValues(attributeValue_pairs)
+                c = attributes.get #NOTE h -> head node attributes
+                chunkId = c('chunkId_')
+                children_.append(self.node._make([None,c('head_'),None,c('poslcat_'),self.features(c('lemma_')\
+                    if c('lemma_') else (c('head_') or '') ,c('cat_'),c('gen_'),c('num_'),c('per_'),c('case_'),
+                    c('vib_'),c('tam_')),c('vpos_'),c('name_'),c('drel_'),c('parent_'),c('pid_'),c('chunkId_'),
+                    None,c('mtype_'),c('troot_'),c('coref_'),c('stype_'),c('voicetype_'),c('posn_')]))
+                self.modifierModified[c('chunkId_')] = c('parent_')
+            elif nodeInfo[0].replace(".",'',1).isdigit(): # word 
+                assert (len(nodeInfo) == 4) and (nodeInfo[1] and nodeInfo[2] != '') # FIXME
+                self.id_ += 1
+                pos_ = nodeInfo[2].encode("utf-8").decode("ascii",'ignore').encode("ascii")
+                wordForm_ = re.sub("'", "`", nodeInfo[1])
+		self.instances.setdefault(wordForm_, 0)
+		self.instances[wordForm_] += 1
+                attributeValue_pairs = self.FSPairs(nodeInfo[3][4:-1], wordForm_)
+                attributes = self.updateFSValues(attributeValue_pairs, wordForm_, pos_)
+                c = attributes.get #NOTE c -> child node attributes
+                self.nodeIndex[self.id_] = c('name_')
+                children_.append(self.node._make([str(self.id_),wordForm_,pos_,c('poslcat_'),self.features(c('lemma_')\
+                    if c('lemma_') else wordForm_ ,c('cat_'),c('gen_'),c('num_'),c('per_'),c('case_'),c('vib_'),
+                    c('tam_')),c('vpos_'),c('name_'),None,None,None,chunkId,None,c('mtype_'),
+                    c('troot_'),c('coref_'),None, None, c('posn_')]))
+            else: 
+                self.nodeList.append(children_)
+                children_ = list()
+        return self
+
+    def FSPairs (self, FS, terminal=False) :
+        feats = dict()
+        for feat in FS.split():
+            if "=" not in feat:continue
+            feat = re.sub("af='+","af='",feat.replace("dmrel=",'drel='))
+            assert len(feat.split("=")) == 2
+            attribute,value = feat.split("=")
 	
-	def __init__ (self, sentence):
+	    if (value[0] == value[-1]) and (value[0] in ["'", '"']):
+            	value = re.sub("'", "`", value[1:-1])
+	    else:
+		value = re.sub("'", "`", value)
+		
+	    if (terminal) and (attribute == "name"):
+		icount = self.instances[terminal]
+		value = terminal if icount < 2 else "%s%s" % (terminal, icount)
 
-		self.id_ = int()
-		self.nodeList = list()
-		self.nodeIndex = dict()
-		self.sentence = sentence
-		self.modifierModified = dict()
-		self.node = namedtuple('node', 
-						['id', 'head', 'pos', 'poslcat', 'af', 'vpos', 'name','drel','parent', 'pid',
-						'chunkId', 'chunkType', 'mtype', 'troot', 'coref', 'stype','voicetype', 'posn'])
-		self.features = namedtuple('features',
-							['lemma','cat','gen','num','per','case','vib','tam'])
+            feats[attribute] = value
+		
+        return feats
 
-	def getAnnotations (self):
-		children_ = list()
-		chunkId = str()
-		for line in self.sentence:
-			nodeInfo = line.decode("utf-8").split("\t")
-			if nodeInfo[0].isdigit(): # chunk
-				assert len(nodeInfo) == 4 # no need to process trash! FIXME
-				attributeValue_pairs = self.FSPairs(nodeInfo[3][4:-1])
-				attributes = self.updateFSValues(attributeValue_pairs)
-				c = attributes.get #NOTE h -> head node attributes
-				chunkId = c('chunkId_')
-				children_.append(self.node._make([None,c('head_'),None,c('poslcat_'),self.features(c('lemma_')\
-					if c('lemma_') else (c('head_') or '') ,c('cat_'),c('gen_'),c('num_'),c('per_'),c('case_'),
-					c('vib_'),c('tam_')),c('vpos_'),c('name_'),c('drel_'),c('parent_'),c('pid_'),c('chunkId_'),
-					None,c('mtype_'),c('troot_'),c('coref_'),c('stype_'),c('voicetype_'),c('posn_')]))
-				self.modifierModified[c('chunkId_')] = c('parent_')
-			elif nodeInfo[0].replace(".",'',1).isdigit(): # word 
-				assert (len(nodeInfo) == 4) and (nodeInfo[1] and nodeInfo[2] != '') # FIXME
-				self.id_ += 1
-				pos_ = nodeInfo[2].encode("utf-8").decode("ascii",'ignore').encode("ascii")
-				wordForm_ = nodeInfo[1]
-				attributeValue_pairs = self.FSPairs(nodeInfo[3][4:-1])
-				attributes = self.updateFSValues(attributeValue_pairs, wordForm_, pos_)
-				c = attributes.get #NOTE c -> child node attributes
-				self.nodeIndex[self.id_] = c('name_')
-				children_.append(self.node._make([str(self.id_),wordForm_,pos_,c('poslcat_'),self.features(c('lemma_')\
-					if c('lemma_') else wordForm_ ,c('cat_'),c('gen_'),c('num_'),c('per_'),c('case_'),c('vib_'),
-					c('tam_')),c('vpos_'),c('name_'),None,None,None,chunkId,None,c('mtype_'),
-					c('troot_'),c('coref_'),None, None, c('posn_')]))
-			else: 
-				self.nodeList.append(children_)
-				children_ = list()
-		return self
-
-	def FSPairs (self, FS) :
-		feats = dict()
-		for feat in FS.split():
-			if "=" not in feat:continue
-			feat = re.sub("af='+","af='",feat.replace("dmrel=",'drel='))
-			assert len(feat.split("=")) == 2
-			attribute,value = feat.split("=")
-			feats[attribute] = re.sub("'|\"",'',value)
-		return feats
-
-	def morphFeatures (self, AF, word, tag):
-		"LEMMA,CAT,GEN,NUM,PER,CASE,VIB,TAM"
-		#assert len(AF[:-1].split(",")) == 8 # no need to process trash! FIXME
-		if tag == 'SYM':
-			lemma_,cat_ = word, 'punc'
-			gen_,num_,per_,case_,vib_,tam_ = [''] * 6
-		else:	
-			lemma_,cat_,gen_,num_,per_,case_,vib_,tam_ = AF.split(",")
-		if len(lemma_) > 1: lemma_ = lemma_.strip("'")
-		return lemma_.strip("'"),cat_,gen_,num_,per_,case_,vib_,tam_.strip("'")
-	
-	def updateFSValues (self, attributeValue_pairs, word=None, tag=None):
-		attributes = dict(zip(['head_','poslcat_','af_','vpos_','name_','drel_','parent_','pid_','mtype_','troot_','chunkId_',
-					'coref_','stype_','voicetype_','posn_'], [None] * 15))
-		attributes.update(dict(zip(['lemma_','cat_','gen_','num_','per_','case_','vib_','tam_'], [''] * 8)))
-		for key,value in attributeValue_pairs.items():
-			if key == "af":
-				attributes['lemma_'],attributes['cat_'],attributes['gen_'],attributes['num_'],\
-				attributes['per_'],attributes['case_'],attributes['vib_'],attributes['tam_'] = \
-					self.morphFeatures (value, word, tag)
-			elif key == "drel":
-				assert len(value.split(":",1)) == 2 # no need to process trash! FIXME
-				attributes['drel_'], attributes['parent_'] = re.sub("'|\"",'',value).split(":",1)
-				assert attributes['drel_'] and attributes['parent_'] != "" # no need to process trash! FIXME
-			else:
-				variable = str(key) + "_"
-				if variable == "name_": attributes['chunkId_'] = re.sub("'|\"",'',value)
-				attributes[variable] = re.sub("'|\"",'',value)
-		return attributes
+    def morphFeatures (self, AF, word, tag):
+        "LEMMA,CAT,GEN,NUM,PER,CASE,VIB,TAM"
+        #assert len(AF[:-1].split(",")) == 8 # no need to process trash! FIXME
+        if tag == 'SYM':
+            lemma_,cat_ = word, 'punc'
+            gen_,num_,per_,case_,vib_,tam_ = [''] * 6
+        else:   
+            lemma_,cat_,gen_,num_,per_,case_,vib_,tam_ = AF.split(",")
+        if len(lemma_) > 1: lemma_ = lemma_.strip("'")
+        return lemma_.strip("'"),cat_,gen_,num_,per_,case_,vib_,tam_.strip("'")
+    
+    def updateFSValues (self, attributeValue_pairs, word=None, tag=None):
+        attributes = dict(zip(['head_','poslcat_','af_','vpos_','name_','drel_','parent_','pid_','mtype_','troot_','chunkId_',
+                    'coref_','stype_','voicetype_','posn_'], [None] * 15))
+        attributes.update(dict(zip(['lemma_','cat_','gen_','num_','per_','case_','vib_','tam_'], [''] * 8)))
+        for key,value in attributeValue_pairs.items():
+            if key == "af":
+                attributes['lemma_'],attributes['cat_'],attributes['gen_'],attributes['num_'],\
+                attributes['per_'],attributes['case_'],attributes['vib_'],attributes['tam_'] = \
+                    self.morphFeatures (value, word, tag)
+            elif key == "drel":
+                assert len(value.split(":",1)) == 2 # no need to process trash! FIXME
+                attributes['drel_'], attributes['parent_'] = re.sub("'|\"","",value).split(":",1)
+                assert attributes['drel_'] and attributes['parent_'] != "" # no need to process trash! FIXME
+            else:
+                variable = "%s_" % str(key)
+                if variable == "name_": attributes['chunkId_'] = value.strip("'\"")
+                attributes[variable] = value.strip("'")
+        return attributes
